540921b38ab8ab41bcfaa6e132efb465e89cacd2
Amul-Thantharate <amulthantharate@gmail.com>
2023-12-06 19:36:55 +0530

    Intial commit

---
+++ Eks-terraform/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "github-actions-cicd-project" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
